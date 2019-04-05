e53f008aeb5ab04a4deab60b5ab0e412ec70585f
Ammon Smith <ammon.i.smith@gmail.com>
2019-04-05 18:27:43 -0400

    Fix whitespace.

--- lib/Text_Wiki/Text/Wiki/Parse/Default/Freelink.php
+++ lib/Text_Wiki/Text/Wiki/Parse/Default/Freelink.php
@@ -1,29 +1,29 @@
 <?php
 
 /**
-* 
+*
 * Parses for wiki freelink text.
-* 
+*
 * @category Text
-* 
+*
 * @package Text_Wiki
-* 
+*
 * @author Paul M. Jones <pmjones@php.net>
 * @author Michal Frackowiak
-* 
+*
 * @license LGPL
-* 
+*
 * @version $Id$
-* 
+*
 */
 
 /**
-* 
+*
 * Parses for freelinked page links.
-* 
+*
 * This class implements a Text_Wiki_Parse to find source text marked as a
 * wiki freelink, and automatically create a link to that page.
-* 
+*
 * A freelink is any page name not conforming to the standard
 * StudlyCapsStyle for a wiki page name.  For example, a page normally
 * named MyHomePage can be renamed and referred to as ((My Home Page)) --
@@ -33,32 +33,32 @@
 * ((MyHomePage|My Home Page#Section1)).
 *
 * @category Text
-* 
+*
 * @package Text_Wiki
-* 
+*
 * @author Paul M. Jones <pmjones@php.net>
 * @author Michal Frackowiak
-* 
+*
 */
 
 class Text_Wiki_Parse_Freelink extends Text_Wiki_Parse {
 
     /**
-    * 
+    *
     * Constructor.  We override the Text_Wiki_Parse constructor so we can
     * explicitly comment each part of the $regex property.
-    * 
+    *
     * @access public
-    * 
+    *
     * @param object &$obj The calling "parent" Text_Wiki object.
-    * 
+    *
     */
-    
+
     function Text_Wiki_Parse_Freelink(&$obj)
     {
-    
+
         parent::Text_Wiki_Parse($obj);
-        
+
 //        $this->regex =
 //            '/' .                                                   // START regex
 //            "\\[\\[\\[" .                                               // double open-parens
@@ -77,7 +77,7 @@ class Text_Wiki_Parse_Freelink extends Text_Wiki_Parse {
 //            "()\\]\\]\\]" .                                           // double close-parens
 //            '/';                                                   // END regex
 
-		   $this->regex =
+           $this->regex =
             '/' .                                                   // START regex
             "\\[\\[\\[" .                                               // double open-parens
             "(" .                                                   // START freelink page patter
@@ -99,16 +99,16 @@ class Text_Wiki_Parse_Freelink extends Text_Wiki_Parse {
     }
 
     /**
-    * 
+    *
     * Generates a replacement for the matched text.  Token options are:
-    * 
+    *
     * 'page' => the wiki page name (e.g., HomePage).
-    * 
+    *
     * 'text' => alternative text to be displayed in place of the wiki
     * page name.
-    * 
+    *
     * 'anchor' => a named anchor on the target wiki page
-    * 
+    *
     * @access public
     *
     * @param array &$matches The array of matches from parse().
@@ -117,7 +117,7 @@ class Text_Wiki_Parse_Freelink extends Text_Wiki_Parse {
     * the source text, plus any text priot to the match.
     *
     */
-    
+
     function process(&$matches)
     {
         // use nice variable names
@@ -126,12 +126,12 @@ class Text_Wiki_Parse_Freelink extends Text_Wiki_Parse {
         $anchor = $matches[2];
 
         if($page{0} == '_'){
-        		$page = substr($page, 1);
-        		$nonbr = true;	
+                $page = substr($page, 1);
+                $nonbr = true;
         }
         // check if references to another site too.
         $site = null;
-        
+
         if(strpos($page, '::')){
             $site = substr($page,0,strpos($page, '::'));
             $site = WDStringUtils::toUnixName($site);
@@ -140,40 +140,40 @@ class Text_Wiki_Parse_Freelink extends Text_Wiki_Parse {
                 $page = $site;
             }
         }
-        
+
         // is the page given a new text appearance?
         if (trim($text) == '') {
             // no
             $text = $page;
             if(strpos($text, ':') != false){
-				$text = substr($text, strpos($text, ':')+1);
-        	}
+                $text = substr($text, strpos($text, ':')+1);
+            }
         }elseif(trim($text) == '|'){
-        	// get $text from the page title (if exists)
-        	$textFromTitle = true;	
-        
+            // get $text from the page title (if exists)
+            $textFromTitle = true;
+
         } else {
             // yes, strip the leading | character
             $text = substr($text, 1);
         }
-        
-        
-        
+
+
+
         // MF: 'purify' the page name
         $page = WDStringUtils::toUnixName($page);
-        
+
         // set the options
         $options = array(
-            'site'	=> $site,
+            'site'    => $site,
             'page'   => $page,
             'text'   => $text,
             'anchor' => $anchor,
             'textFromTitle' => $textFromTitle
         );
         if($nonbr){
-        		$options['nonbr']=true;	
+                $options['nonbr']=true;
         }
-        
+
         // return a token placeholder
         return $this->wiki->addToken($this->rule, $options);
     }
